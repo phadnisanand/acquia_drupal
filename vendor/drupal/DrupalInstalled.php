924c6a9382faeb39525c13bcd6a5b61a00d05bcb
Anand Phadis <phadnisanandhyd@gmail.com>
2026-08-28 08:28:04 +0530

    commit

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '58859e5937ed9b99';
+  public const string VERSIONS_HASH = '2d762ce027b1704d';
 
 }
